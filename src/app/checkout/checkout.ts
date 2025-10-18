029f72629da6b667f2b73662a27793867acaaea4
Ella Wei <ellawei8207@gmail.com>
2025-10-18 18:53:25 -0500

    i give up

--- src/app/checkout/checkout.ts
+++ src/app/checkout/checkout.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
-  templateUrl: './checkout.html',
-  styleUrl: './checkout.scss'
+  template: `
+    <h1>Checkout</h1>
+    <p>Your shopping cart items will appear here.</p>
+  `,
+  styles: [`
+    h1 { margin-bottom: 1rem; }
+  `]
 })
-export class Checkout {
-
-}
+export class Checkout {}
